4820ee2d2fc5672214ec31c03d11fc6d5b52d84d
duc <hohuynhduca7bh123@gmail.com>
2020-08-01 22:51:34 +0700

    finish sum all number in a  range

---
+++ SumAllNumbersInARange/main.js
@@ -0,0 +1,19 @@
+function sumAll(arr){
+    let min, max;
+
+    if (arr[0] > arr[1]) {
+        max = arr[0];
+        min = arr[1];
+    }else{
+        max = arr[1];
+        min = arr[0];
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++){
+        sum+=i;
+    }
+
+    return sum;
+}     
+
+console.log(sumAll([4,4]));
