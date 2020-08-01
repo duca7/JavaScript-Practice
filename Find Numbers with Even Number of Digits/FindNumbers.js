d32bb2e78832bd24f7776467aa58ff03a1fc3571
duc <hohuynhduca7bh123@gmail.com>
2020-08-01 21:56:46 +0700

    init sum all number in a range

---
+++ Find Numbers with Even Number of Digits/FindNumbers.js
@@ -0,0 +1,19 @@
+// var nums = [12, 6 , 2567];
+
+function countNumofDigits(n){
+    var count = 0;
+    while (n != 0) {
+       n = n / 10
+       count++;
+    }
+    return count;
+}
+
+function numberDigits(nums){
+    for (let i = 0; i < nums.length; i++) {
+     if () {
+         
+     }
+        
+ }
+}
